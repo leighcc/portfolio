057f07e8f7e322edb9643fab87b59c98c34a1a62
leighcc <1053696417@qq.com>
2016-07-13 16:03:53 +0800

    rolling box

--- DEMO/gulpfile.js
+++ DEMO/gulpfile.js
@@ -1,18 +1,42 @@
 var gulp = require('gulp'),
-	cleanCSS = require('gulp-clean-css'),
-	browserSync = require('browser-sync').create();
+	uglify = require('gulp-uglify'),
+	livereload = require('gulp-livereload'),
+	minifyCSS = require('gulp-minify-css'),
+	prefix = require('gulp-autoprefixer'),
+	// concatCSS = require('gulp-concat-css'),
+	sass = require('gulp-ruby-sass');
 
-gulp.task('minify-css', function () {
-	gulp.src('pending/main.css')
-		.pipe(cleanCSS())
-		.pipe(gulp.dest('style'))
-		.pipe(browserSync.stream());
+// modifies styles
+gulp.task('styles', function () {
+	console.log('starting styles');
+	gulp.src('CDG/css/*.css')
+		// .pipe(sass({ style: 'expanded'}))
+		.pipe(prefix('ios >= 6', 'android >= 4.4'))
+		.pipe(minifyCSS())
+		.pipe(gulp.dest('CDG/build/css/'))
+		.pipe(livereload());
 });
 
-gulp.task('default', ['minify-css'], function () {
-	browserSync.init({
-		server: "./"
-	});
-	gulp.watch('pending/main.css', ['minify-css']);
-	gulp.watch('index.html').on('change', browserSync.reload);
+// // modifies scripts
+// gulp.task('scripts', function () {
+// 	console.log('starting scripts');
+// 	gulp.src('pending/**/*.js')
+// 		.pipe(uglify())
+// 		.pipe(gulp.dest('js/'))
+// 		.pipe(livereload());
+// });
+
+// watches files
+gulp.task('watch', function () {
+	livereload.listen();
+	
+	gulp.watch('CDG/css/*.css', ['styles']);
+
+	gulp.watch('CDG/index.html')
+		.on('change', livereload.changed);
 });
+
+// //runs all gulp tasks
+// gulp.task('default', function () {
+// 	gulp.start('styles', 'scripts');
+// });
